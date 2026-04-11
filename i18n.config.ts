075be82b18660d41022db32916ff9f3c0eca50f1
Ahmed NAgy <140100710+AhmedNAgy25@users.noreply.github.com>
2026-04-11 17:24:09 +0200

    Revert "Initialization v2"

--- i18n.config.ts
+++
@@ -1,9 +0,0 @@
-import type { I18nConfig } from 'next-i18next/proxy'
-
-const i18nConfig: I18nConfig = {
-  supportedLngs: ['en', 'ar'],
-  fallbackLng: 'en',
-  defaultNS: 'common',
-}
-
-export default i18nConfig
--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  turbopack: {
-    resolveAlias: {
-      "@": "./src",
-    },
-  },
-  experimental: {
-    turbopackFileSystemCacheForDev: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
