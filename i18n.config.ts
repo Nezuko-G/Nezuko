6f00bf35c4f02ecb3684de4c650187b337680036
Ahmed Nagy <ahmednagy25t@gmail.com>
2026-04-11 15:19:20 +0200

    feat: add internationalization support with Arabic and English translations, user DTO starter code and mapping functions, add .gitattributes

---
+++ i18n.config.ts
@@ -0,0 +1,9 @@
+import type { I18nConfig } from 'next-i18next/proxy'
+
+const i18nConfig: I18nConfig = {
+  supportedLngs: ['en', 'ar'],
+  fallbackLng: 'en',
+  defaultNS: 'common',
+}
+
+export default i18nConfig
