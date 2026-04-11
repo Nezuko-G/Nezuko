6fe4a90b349b078e8a276c507e2f80baec80dc53
Ahmed Nagy <ahmednagy25t@gmail.com>
2026-04-11 20:03:42 +0200

    *

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -9,17 +9,12 @@ export default getRequestConfig(async ({ requestLocale }) => {
     locale = routing.defaultLocale
   }
 
-  // Load available namespace files
-  const messages: Record<string, Record<string, unknown>> = {}
+  const common = (await import(`../../messages/${locale}/common.json`)).default
   
-  // Common namespace (always required)
-  messages.common = (await import(`../../messages/${locale}/common.json`)).default
-  
-  // Home namespace
-  messages.home = (await import(`../../messages/${locale}/home.json`)).default
-
   return {
     locale: locale as typeof routing.locales[number],
-    messages
+    messages: {
+      common
+    }
   }
 })
