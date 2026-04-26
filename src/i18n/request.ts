a16284a541bfaff4ca5ce2aa2daac278f968a6b1
Abdelrahman M Selim <engabdo880@gmail.com>
2026-04-27 01:15:50 +0300

    add dashboard route

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -13,6 +13,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
   const landing = (await import(`../../messages/${locale}/landing.json`)).default
   const auth = (await import(`../../messages/${locale}/auth.json`)).default
   const bookDemo = (await import(`../../messages/${locale}/bookDemo.json`)).default
+  const dashboard = (await import(`../../messages/${locale}/dashboard.json`)).default
 
 
   return {
@@ -21,7 +22,8 @@ export default getRequestConfig(async ({ requestLocale }) => {
       common,
       landing,
       auth,
-      bookDemo
+      bookDemo,
+      dashboard,
     }
   }
 })
